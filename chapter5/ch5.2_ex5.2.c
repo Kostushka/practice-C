4a00081dc737a933973ffadc724ce2ca3c959aff
Kostushka <nastyatery47@yandex.ru>
2023-01-24 15:12:25 +0300

    Added chapter5

---
+++ chapter5/ch5.2_ex5.2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 10
+
+// Упр. 5.2
+// getfloat - для получения вещественных чисел из входного потока
+
+int getfloat (double *);
+int getch (void);
+void ungetch (int c);
+
+int main (void) {
+
+	double num[MAX];
+	int i;
+
+	for (i = 0; i < MAX && getfloat(&num[i]) != EOF; i++) {
+		printf("%d: %g\n", i, num[i]);
+	}
+
+	return 0;
+}
+
+int getfloat (double *n) {
+	int c, sign;
+	double pow;
+
+	// пропускаем пробельные символы
+	while (isspace(c = getch()));
+
+	// записываем не число и выходим
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c);
+		return 0;
+	}
+
+	// записываем знак, запрашиваем следующий символ
+	sign = c == '-' ? -1 : 1;
+	if (c == '-' || c == '+') {
+		c = getch();
+	}
+
+	// записываем число, пока цифровые символы
+	for (*n = 0; isdigit(c); c = getch()) {
+		*n = *n * 10 + (c - '0');
+	}
+
+	// если точка, продолжаем записывать число, пока после точки идут цифровые символы
+	// записываем десятки
+	pow = 1;
+	if (c == '.') {
+		while (isdigit(c = getch())) {
+			*n = *n * 10 + (c - '0');
+			pow = pow * 10;
+		}
+	}
+
+	// записываем окончательное вещественное число
+	*n = sign * *n / pow;
+
+	// нецифровой символ запоминаем
+	if (c != EOF) {
+		ungetch(c);
+	}
+	// возвращаем на случай, если EOF
+	return c;	
+}
+
+
+
+char buf[MAX];
+int b = 0;
+
+int getch (void) {
+	return b > 0 ? buf[--b] : getchar();
+}
+
+void ungetch (int c) {
+	if (b < MAX) {
+		buf[b++] = c;
+	}
+}
---
+++ chapter5/chapter5.2.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 10
+// 5.2 Указатели и аргументы функций
+
+void swap (int a, int b);
+void swap2 (int *, int *);
+
+int getint(int *);
+int getch (void);
+void ungetch (int c);
+
+int main (void) {
+	// вызываем функцию для получения чисел из входного потока
+	// числа записываем в массив с ячейками для каждого числа
+	int numbers[MAX], i;
+	for (i = 0; i < MAX && getint(&numbers[i]) != EOF; i++) {
+		printf("%d: %d\n", i, numbers[i]);
+	}
+
+	int x = 23, y = 67;
+	printf("%d %d\n", x, y);
+	
+	swap(x, y); // аргументы передаются по значению
+	printf("нет изменений\n");
+	printf("%d %d\n", x, y);
+	
+	swap2(&x, &y); // передаем адреса
+	printf("поменяли местами\n");
+	printf("%d %d\n", x, y);
+	
+	return 0;
+}
+
+// НЕПРАВИЛЬНО
+void swap (int a, int b) {
+	// здесь меняются локальные переменные
+	int c;
+	c = a;
+	a = b;
+	b = c;
+}
+
+// ПРАВИЛЬНО
+void swap2 (int *a, int *b) {
+	// работаем с указателями (адресами)
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+// получить целое число из входного потока
+int getint (int *p) {
+	int c, sign;
+	
+	// пропускаем пробелы
+	while (isspace(c = getch()));
+
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		// бесконечный цикл, если знак не пробельный
+		// по умолчанию в массиве всякая ерунда
+		ungetch(c); // не цифра
+		return 0;
+	}
+	
+	// запомниаем и пропусаем знак
+	sign = c == '-' ? -1 : 1;
+	if (c == '-' || c == '+') {
+		c = getch();
+	}
+
+	// записываем по адресу число
+	for (*p = 0; isdigit(c); c = getch()) {
+		*p = *p * 10 + (c - '0');
+	}
+	*p *= sign;
+
+	// если не конец файла запоминаем
+	if (c != EOF) {
+		ungetch(c);
+	}
+	// для того, чтобы иметь возможность вернуть EOF
+	return c;
+	
+}
+
+char buf[MAX];
+int b = 0;
+
+int getch (void) {
+	return b > 0 ? buf[--b] : getchar();
+}
+
+void ungetch (int c) {
+	if (b < MAX) {
+		buf[b++] = c;
+	}
+}
