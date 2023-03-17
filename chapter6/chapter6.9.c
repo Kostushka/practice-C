9016a30ab81e72186344c75378210667a97c2470
Kostushka <nastyatery47@yandex.ru>
2023-03-17 21:44:07 +0300

    Added chapter6.9.c

--- chapter6/chapter6.9.c
+++ chapter6/chapter6.9.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 // 6.9 Битовые поля
 // Пример: набор однобитовых флажков-переключателей
@@ -9,22 +10,30 @@
 // или
 // enum {KEYWORD = 01, EXTERNAL = 02, STATIC = 04};
 
-unsigned int flags = ~0;
-// установить равным 1 биты EXTERNAL и STATIC в flags
-flags |= EXTERNAL | STATIC;
-// установить равным 0 биты EXTERNAL и STATIC в flags
-flags &= ~(EXTERNAL | STATIC);
 
-// НО можно обойтись и без поразрядных операций
-// внутри "слова" можно задать битовое поле - последовательность битов
-struct {
-	unsigned int is_keyword : 1;
-	unsigned int is_external : 1;
-	unsigned int is_static : 1;
-} flags2;
-
-flags2.is_external = flags2.is_static = 0;
 
 int main (void) {
+
+	unsigned int flags = 0;
+	
+	// установить равным 1 биты EXTERNAL и STATIC в flags
+	flags |= EXTERNAL | STATIC;
+	printf("%x\n", flags);
+	
+	// установить равным 0 биты EXTERNAL и STATIC в flags
+	flags &= ~(EXTERNAL | STATIC);
+	printf("%o %x\n", ~(EXTERNAL | STATIC), flags);
+
+	// НО можно обойтись и без поразрядных операций
+	// внутри "слова" можно задать битовое поле - последовательность битов
+	struct {
+		unsigned int is_keyword : 1;
+		unsigned int is_external : 1;
+		unsigned int is_static : 1;
+	} flags2;
+
+	flags2.is_external = flags2.is_static = 0;
+	printf("%x\n", flags2.is_keyword);
+	
 	return 0;
 }
