42b92a7fbf2866d0092be580ee6b1f0fca02b077
Kostushka <nastyatery47@yandex.ru>
2023-05-24 20:09:43 +0300

    Fixed swap

--- chapter4/ch4.11.2_ex4.14.c
+++ chapter4/ch4.11.2_ex4.14.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#define swap(t, x, y) {t = x; x = y; y = t;}
+#define swap(t, x, y) {t c; c = x; x = y; y = c;}
 
 // Упр. 4.14
 // Макрос swap обменивает местами значения двух аргументов типа t
 
 int main (void) {
-	int x, y, t;
-	t = 0;
+	int x, y;
 	x = 5;
 	y = 8;
 
 	printf("%d %d\n", x, y);
-	swap(t, x, y);
+	swap(int, x, y);
 	printf("%d %d\n", x, y);
 	
 	return 0;
